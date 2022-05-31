aa75e9e771f137a2d170ea67ce1414907d1c4c7e
KilianAlves <alveskilian@gmail.com>
2022-05-31 14:13:54 +0200

    ajout methode findAll dans TvShowCollection.php

--- src/Entity/Collection/TvShowCollection.php
+++ src/Entity/Collection/TvShowCollection.php
@@ -8,5 +8,26 @@ use Entity\tvshow;
 
 class TvShowCollection
 {
+    /**
+     * La methode cree un tableau avec tout du tvShow
+     *
+     * @return tvshow[]
+     */
+    public static function findAll(): array
+    {
 
+        #recupere les nom des artistes
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, name, originalName, homepage, overview, posterId
+            FROM tvshow
+            ORDER BY name
+            SQL
+        );
+        $stmt->execute();
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, tvshow::class);
+
+        return $stmt->fetchAll();
+    }
 }
