a7f0ef5039c6cdb3d5e4b186ec2aaad22652c4dd
KilianAlves <alveskilian@gmail.com>
2022-06-01 17:47:41 +0200

    Ajout de la methode findByIdEpisode

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -2,6 +2,10 @@
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Episode
 {
     private int $id;
@@ -49,4 +53,24 @@ class Episode
     {
         return $this->overview;
     }
+
+    public static function findByIdEpisode(int $id): Episode {
+        $AlbumEtDate = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, name,overview,episodeNumber,seasonId
+            FROM episode
+            WHERE id = ? 
+            SQL
+        );
+
+        #execute le sql
+        $AlbumEtDate->execute([$id]);
+        $AlbumEtDate->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Episode::class);
+        $artist = $AlbumEtDate->fetch();
+
+        if ($artist === false) {
+            throw new EntityNotFoundException("Artist with id $id not found");
+        }
+        return $artist;
+}
 }
