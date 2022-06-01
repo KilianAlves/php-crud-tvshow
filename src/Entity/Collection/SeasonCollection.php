70ec149c755e21bf6d16e8f086790cc95c67379c
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-01 09:36:23 +0200

    ajout de la methode finByTvShowId dans SeasonCollection.php

--- src/Entity/Collection/SeasonCollection.php
+++ src/Entity/Collection/SeasonCollection.php
@@ -9,5 +9,24 @@ use Entity\season;
 
 class SeasonCollection
 {
-    
+    /**
+     * La methode cree un tableau avec tout de tvShow
+     *
+     * @return season[]
+     */
+    public static function findByTvShowId(int $tvShowId): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT name, seasonNumber, posterId, id
+            FROM season s
+            WHERE tvShowId=:tvShowId
+            ORDER BY seasonNumber;
+            SQL
+            );
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, season::class);
+        $stmt->execute(['tvShowId' => $tvShowId]);
+
+        return $stmt->fetchAll();
+    }
 }
