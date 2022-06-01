fc32ed5d13599ac3f081b789b2dd19336c4a480c
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-01 10:11:09 +0200

    ajout de la methode findBySeasonId dans EpisodeCollection.php

--- src/Entity/Collection/EpisodeCollection.php
+++ src/Entity/Collection/EpisodeCollection.php
@@ -9,5 +9,24 @@ use Entity\Episode;
 
 class EpisodeCollection
 {
+    /**
+     * La methode cree un tableau avec tout de episode
+     *
+     * @return episode[]
+     */
+    public static function findBySeasonId(int $seasonId): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT name, overview, episodeNumber, id
+            FROM episode
+            WHERE seasonId=:seasonId
+            ORDER BY episodeNumber;
+            SQL
+        );
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Episode::class);
+        $stmt->execute(['seasonId' => $seasonId]);
 
+        return $stmt->fetchAll();
+    }
 }
