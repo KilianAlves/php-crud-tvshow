e72583b4468be42e89573b145b21b9716b944356
KilianAlves <alveskilian@gmail.com>
2022-06-01 17:49:00 +0200

    Ajout de la methode findByIdSeason

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Season
 {
     private int $id;
@@ -51,5 +55,24 @@ class Season
         return $this->name;
     }
 
+    public static function findByIdSeason(int $idSeason): Season
+    {
+        $LaSeason = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id,name,posterId,seasonNumber,tvShowId
+            FROM season
+            WHERE id = ? 
+            SQL
+        );
 
+        #execute le sql
+        $LaSeason->execute([$idSeason]);
+        $LaSeason->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Season::class);
+        $info = $LaSeason->fetch();
+
+        if ($info === false) {
+            throw new EntityNotFoundException("Artist with id $idSeason not found");
+        }
+        return $info;
+    }
 }
