b980754564115c021ad156190c3a475ac6035c7b
KilianAlves <alveskilian@gmail.com>
2022-06-02 13:19:55 +0200

    Fin de la page des episodes

--- public/season.php
+++ public/season.php
@@ -15,18 +15,27 @@ if (!isset($_GET["seasonId"]) || !ctype_digit($_GET["seasonId"])) {
 }
 
 $seasonId = $_GET["seasonId"];
-$seasonId = intval($seasonId);
+$seasonId = (int)$seasonId;
 
-$season = Episode::findByIdEpisode($seasonId);
-$serie = Season::findByIdSeason($season->getSeasonId());
-$show = TvShow::findById($serie->getTvShowId());
+$season = Season::findByIdSeason($seasonId);
+$serie = TvShow::findById($season->getTvShowId());
 
-$webPage = new AppWebPage("Série Tv : {$show->getName()} {$serie->getName()}"); #crée page web avec titre en nom
+
+$webPage = new AppWebPage("Série Tv : {$serie->getName()} {$season->getName()}"); #crée page web avec titre en nom
 
 $listeEpisode = EpisodeCollection::findBySeasonId($seasonId);
 
 #la saison a mettre ici
+$photo = $webPage::escapeString("{$season->getPosterId()}");
+$nameSerie =$webPage::escapeString("{$serie->getName()}");
+$nameSeason =$webPage::escapeString("{$season->getName()}");
 
+$webPage->appendContent("<div>");
+$webPage->appendContent("<div><img src='poster.php?id={$photo}'></div>");
+$webPage->appendContent("<div>");
+$webPage->appendContent("<h3><a>{$nameSerie}</a></h3>");
+$webPage->appendContent("<h3>{$nameSeason}</h3>");
+$webPage->appendContent("</div></div>");
 
 #Les episode de la season
 foreach ($listeEpisode as $Episode) {
