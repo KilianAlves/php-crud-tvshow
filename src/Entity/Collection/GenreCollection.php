e3428343127a80583b47208e8edaa0e1f941f844
KilianAlves <alveskilian@gmail.com>
2022-06-02 13:53:23 +0200

    Ajout de la methode findAllGenre dans GenreCollection

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -2,7 +2,25 @@
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+
 class GenreCollection
 {
+    public static function findAllGenre(): array
+    {
 
+        #recupere les nom des artistes
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id,genre
+            FROM genre
+            ORDER BY name
+            SQL
+        );
+        $stmt->execute();
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, TvShow::class);
+
+        return $stmt->fetchAll();
+    }
 }
