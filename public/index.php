2b89f89e9bbbc54bbe99406d32cfb375a220db7e
KilianAlves <alveskilian@gmail.com>
2022-06-02 15:16:45 +0200

    Ajout formulaire pour filtrer sur index.php

--- public/index.php
+++ public/index.php
@@ -2,11 +2,25 @@
 
 declare(strict_types=1);
 
+use Entity\Collection\GenreCollection;
 use Html\AppWebPage;
 use Entity\Collection\TvShowCollection;
 
 $webPage = new AppWebPage("Série TV");
 
+#Formulaire
+$webPage->appendContent('<form action="filtrer.php" method="GET">');
+$webPage->appendContent('<select name="filtreId" id="filtreId">');
+$webPage->appendContent('<option value="0">Pas de filtre</option>');
+#Les autres options
+foreach (GenreCollection::findAllGenre() as $genre) {
+    $webPage->appendContent("<option value={$genre->getId()}>{$genre->getName()}</option>");
+}
+$webPage->appendContent("</select>");
+$webPage->appendContent('<input type="submit"></input>');
+$webPage->appendContent("</form>");
+#Fin formulaire
+#Listes des series
 foreach (TvShowCollection::findAll() as $serie) {
     $textname = $webPage::escapeString($serie->getName()); #Titre serie
     $resume = $webPage::escapeString($serie->getOverview()); #Description serie
