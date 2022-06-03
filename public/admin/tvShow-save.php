103d8c446ea537ea57e1b0bf97de7a13e94fc79d
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-03 16:02:29 +0200

    ajout du fichier tvShow-save.php #bug

---
+++ public/admin/tvShow-save.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Entity\Exception\ParameterException;
+use Html\AppWebPage;
+use Entity\TvShow;
+use Html\Form\TvShowForm;
+
+$webPage = new AppWebPage("Admin: TvShow");
+
+try {
+    if (isset($_GET['tvShowId'])) {
+        if (!ctype_digit($_GET['tvShowId'])) {
+            throw new ParameterException("Erreur de parametre");
+        } else {
+            $tvShowId = (int)$_GET['tvShowId'];
+            $tvShow = TvShow::findById($tvShowId);
+        }
+    }
+    $tvShowForm = new TvShowForm($tvShow);
+    $tvShowForm->setEntityFromQueryString();
+    $tvShowForm->getTvShow()->save();
+}
