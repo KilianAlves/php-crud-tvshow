6c5f8051525160aa25c1a7fd6ab675fa1bd5edd5
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-03 14:39:43 +0200

    ajout de la methode escapeString de la class StringEscaper

--- src/Html/StringEscaper.php
+++ src/Html/StringEscaper.php
@@ -5,5 +5,16 @@ namespace Html;
 
 trait StringEscaper
 {
-
+    /**
+     * @param string $chaine
+     * @return string retourne la chaine de charactere proteger
+     */
+    public static function escapeString(?string $chaine): string
+    {
+        if ($chaine == null) {
+            return "";
+        } else {
+            return htmlspecialchars($chaine, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
+        }
+    }
 }
