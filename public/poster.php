3c962c5d95ff5d26e45846d8c9f1e87c5553b4e6
KilianAlves <alveskilian@gmail.com>
2022-06-03 23:00:37 +0200

    Ajout du poster par defaut dans poster.php

--- public/poster.php
+++ public/poster.php
@@ -3,12 +3,16 @@
 declare(strict_types=1);
 
 use Entity\Poster;
+use Entity\Exception;
 
-
-
-
-$poster = Poster::findById((int)$_GET['id']);
+try {
+    $poster = Poster::findById((int)$_GET['id']);
 
 header("Content-Type: image/jpeg");
 
 echo $poster->getJpeg();
+
+} catch (EntityNotFoundException) {
+    header("Location: /image/default.png");
+}
+
