2919c8ebcba671992b6dda22b25b4498e68d660b
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-03 14:27:38 +0200

    correction methode getHtmlForm de la class TvShowForm

--- src/Html/Form/TvShowForm.php
+++ src/Html/Form/TvShowForm.php
@@ -28,12 +28,21 @@ class TvShowForm extends TvShow
     public function getHtmlForm(string $url): string
     {
         $htmlName = ($this->tvShow == null) ? "" : $this->tvShow->getName();
+        $htmlOriginalName = ($this->tvShow == null) ? "" : $this->tvShow->getOriginalName();
+        $htmlHomepage = ($this->tvShow == null) ? "" : $this->tvShow->getHomepage();
+        $htmlOverview = ($this->tvShow == null) ? "" : $this->tvShow->getOverview();
         $htmlId = ($this->tvShow == null) ? "" : $this->tvShow->getId();
 
         return <<<HTML
         <form action="{$url}" method="post">
             <label for="name">Nom</label>
             <input name="name" type="text" value="{$htmlName}" required>
+            <label for="originalName">Nom</label>
+            <input name="originalName" type="text" value="{$htmlOriginalName}" required>
+            <label for="homepage">Nom</label>
+            <input name="homepage" type="text" value="{$htmlHomepage}" required>
+            <label for="overview">Nom</label>
+            <input name="overview" type="text" value="{$htmlOverview}" required>
             <input name="id" type="hidden" value="{$htmlId}">
             <input type="submit" value="Enregistrer">
         </form>
