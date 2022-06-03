6a0000c47855577961f936c7e9bffc5c496bc235
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2022-06-03 14:43:03 +0200

    ajout methode stripTagsAndTrim de la class StringEscaper

--- src/Html/StringEscaper.php
+++ src/Html/StringEscaper.php
@@ -7,7 +7,7 @@ trait StringEscaper
 {
     /**
      * @param string $chaine
-     * @return string retourne la chaine de charactere proteger
+     * @return string retourne la chaine de charactere de la base de donnees proteger
      */
     public static function escapeString(?string $chaine): string
     {
@@ -15,6 +15,21 @@ trait StringEscaper
             return "";
         } else {
             return htmlspecialchars($chaine, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
+        }
+    }
+
+    /**
+     * @param string|null $chaine Chaîne de caractères de l'utilisateur à protéger
+     * @return string Chaîne de caractère sécurisée
+     */
+    public static function stripTagsAndTrim(?string $chaine): string
+    {
+        if ($chaine==null) {
+            return "";
+        } else {
+            $var = trim(strip_tags($chaine));
+            $var = ltrim($var, " ");
+            return rtrim($var, " ");
         }
     }
 }
